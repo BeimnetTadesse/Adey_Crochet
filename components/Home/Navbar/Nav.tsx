cff3903154f8eb4839aff144818eecb0acb575ac
Nina <140155579+BeimnetTadesse@users.noreply.github.com>
2025-06-09 01:03:52 +0300

    Update Nav.tsx

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -61,16 +61,17 @@ const Nav = ({ openNav }: Props) => {
         {/* Button & Theme Toggle */}
         <div className="flex items-center space-x-4">
           <a
-            href="#_"
-            className="box-border relative z-30 inline-flex items-center justify-center w-auto px-5 py-2.5 overflow-hidden font-bold text-white transition-all duration-300 bg-[#8B5E3C] rounded-md cursor-pointer group ring-offset-2 ring-1 ring-[#A67B5B] ring-offset-[#8B5E3C] hover:ring-offset-[#6B4226] ease focus:outline-none"
+          href="#contact"
+          onClick={(e) => handleScroll(e, "contact")}
+          className="box-border relative z-30 inline-flex items-center justify-center w-auto px-5 py-2.5 overflow-hidden font-bold text-white transition-all duration-300 bg-[#8B5E3C] rounded-md cursor-pointer group ring-offset-2 ring-1 ring-[#A67B5B] ring-offset-[#8B5E3C] hover:ring-offset-[#6B4226] ease focus:outline-none"
           >
-            <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-            <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-            <span className="relative z-20 flex items-center text-sm">
-              <FaShoppingBag className="mr-3 w-5 h-5" />
-              Buy Now
-            </span>
-          </a>
+          <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+          <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+          <span className="relative z-20 flex items-center text-sm">
+         <FaShoppingBag className="mr-3 w-5 h-5" />
+          Buy Now
+          </span>
+        </a>
           
           {/* Theme Toggle */}
           <ThemeToggle />
